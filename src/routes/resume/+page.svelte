508e8766c3412e17419d92d4d5b751c07759f71c
Imoni McCorvey <imccorve@gmail.com>
2024-05-23 23:51:25 -0400

    Add resume

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,16 @@
+
+<div class="resume-container">
+    <iframe src="/FullstackEngineer.pdf" class="pdf-container"></iframe>
+</div>
+
+<style>
+.resume-container{
+    display: grid;
+    grid-column: span 12;
+}
+.pdf-container {
+    width: 100%;
+    height: 100vh; /* Full viewport height */
+    border: none;
+  }
+</style>
